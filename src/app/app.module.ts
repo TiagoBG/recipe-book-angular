bfb92d1a6a52eceea060bf28b941cb64622ae816
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-03-28 17:55:18 -0500

    Se inicializa el proyecto y se avanza en la creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipeBookComponent } from './components/RecipeBook/recipe-book.component';
+import { RecipeDetailComponent } from './components/RecipeBook/RecipeDetail/recipe-detail.component';
+import { RecipeItemComponent } from './components/RecipeBook/RecipeItem/recipe-item.component';
+import { RecipeListComponent } from './components/RecipeBook/RecipeList/recipe-list.component';
+import { ShoppingListComponent } from './components/ShoppingList/shopping-list.component';
+import { ShoppingListEditComponent } from './components/ShoppingList/ShoppingListEdit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
